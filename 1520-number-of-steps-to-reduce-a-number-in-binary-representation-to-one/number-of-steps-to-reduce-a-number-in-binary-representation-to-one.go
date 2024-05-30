7a29fbee062f1c1908302ee540c0904d2d5fdb0b
mutaev <i.mutaev@gmail.com>
2024-05-31 01:52:29 +0300

    Time: 86 ms (5.00%) | Memory: 32.1 MB (5.00%) - LeetSync comment

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.go
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.go
@@ -1,7 +1,11 @@
+package main
+
+import "fmt"
+
 func numSteps(s string) int {
-    steps := 0
+	steps := 0
 	i := len(s) - 1
-    
+
 	for i >= 1 {
 		if s[i]-'0' == 0 {
 			steps++
@@ -16,6 +20,7 @@ func numSteps(s string) int {
 	return steps
 }
 
+// AddBinary increases the value of number represented in binary as a string by one
 func addBinary(s string) string {
 	result := ""
 	carry := 1
