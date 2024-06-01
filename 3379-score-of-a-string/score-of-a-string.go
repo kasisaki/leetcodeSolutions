cf982c03abbb2168f8f9956958a55b7026c49460
Islam Mutaev <i.mutaev@gmail.com>
2024-06-02 00:17:00 +0300

    Time: 3 ms (31.39%) | Memory: 2.3 MB (13.64%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.go
+++ 3379-score-of-a-string/score-of-a-string.go
@@ -1,14 +1,15 @@
+func abs(n int) int {
+	if n > 0 {
+		return n
+	}
+	return -n
+}
+
 func scoreOfString(s string) int {
 	sum := 0
-	diff := 0
 
 	for i := 0; i < len(s)-1; i++ {
-		a := rune(s[i])
-		b := rune(s[i+1])
-		if a != b {
-			diff = int(math.Abs(float64(a - b)))
-			sum += diff
-		}
+		sum += int(math.Abs(float64(rune(s[i]) - rune(s[i+1]))))
 	}
 	return sum
 }
