568eafad620c2e254803e68506d273bee8f90f09
Islam Mutaev <i.mutaev@gmail.com>
2024-06-03 00:47:49 +0300

    Time: 24 ms (93.39%) | Memory: 6.5 MB (87.51%) - LeetSync

--- 344-reverse-string/reverse-string.go
+++ 344-reverse-string/reverse-string.go
@@ -1,12 +1,12 @@
 func reverseString(s []byte) {
-	var a, b byte
+	var a byte
 	l := len(s) / 2
 	j := len(s) - 1
 
 	for i := 0; i < l; i++ {
 		a = s[i]
-		b = s[j-i]
-		s[i] = b
-		s[j-i] = a
+		s[i] = s[j]
+		s[j] = a
+        j--
 	}
 }
