e0b4768f3ae5db0ed29d6d79aa0d06d5eade4acd
Islam Mutaev <i.mutaev@gmail.com>
2024-06-04 21:26:12 +0300

    Time: 2 ms (63.35%) | Memory: 2.3 MB (63.04%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.go
@@ -0,0 +1,18 @@
+func longestPalindrome(s string) int {
+	m := make(map[rune]int)
+	for _, c := range s {
+		m[c]++
+	}
+	var singleLetterPresent bool
+	var doubles int
+	for _, v := range m {
+		if v%2 == 1 {
+			singleLetterPresent = true
+		}
+		doubles += v / 2
+	}
+	if singleLetterPresent {
+		return doubles*2 + 1
+	}
+	return doubles * 2
+}
